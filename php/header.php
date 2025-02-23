9f6f4542d4421972c2872e75717c0703173cf608
Pericles <slouis482@gmail.com>
2025-02-23 16:39:28 -0400

    Se realizaon modificaciones en el codigo a nivel de  css

---
+++ php/header.php
@@ -0,0 +1,13 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Heladería Fridu</title>
+  <link rel="stylesheet" href="shttps://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="./stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="formularios.css">
+</head>
+
+<body>
