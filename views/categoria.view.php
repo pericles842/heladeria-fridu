d0ee8c9ca3b41a7b5ad3d953f57f8b79df70a601
Pericles <slouis482@gmail.com>
2025-03-15 13:08:17 -0400

    Ajuste de formualarios

--- views/categoria.view.php
+++ views/categoria.view.php
@@ -10,29 +10,6 @@
           <label for="floatingInput">Agregar Nombre</label>
         </div>
       </div>
-
-      <div class="col d-flex align-items-center gap-4">
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="crear">
-          <label class="form-check-label" for="crear">
-            Crear
-          </label>
-        </div>
-
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="editar">
-          <label class="form-check-label" for="editar">
-            Editar
-          </label>
-        </div>
-
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="eliminar">
-          <label class="form-check-label" for="eliminar">
-            Eliminar
-          </label>
-        </div>
-      </div>
     </div>
 
     <div class="d-flex justify-content-end gap-3">
